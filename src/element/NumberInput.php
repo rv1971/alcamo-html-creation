60bef64bd959dca5f988c4ec56cb26906d359b10
rv1971 <rv1971@web.de>
2022-02-07 12:26:39 +0100

    Add NumberInput

---
+++ src/element/NumberInput.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace alcamo\html_creation\element;
+
+/**
+ * @brief HTML element \<input> of type `number`
+ */
+class NumberInput extends Input
+{
+    public const TYPE = 'number';
+}
