bcaa81d2845229fd68da8a4a09e98fc9979ea580
RV1971 <rv1971@web.de>
2022-07-16 17:15:43 +0200

    Add Style

---
+++ src/element/Style.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace alcamo\html_creation\element;
+
+/**
+ * @brief HTML element \<style>
+ */
+class Style extends AbstractSpecificElement
+{
+    public const TAG_NAME = "style";
+}
